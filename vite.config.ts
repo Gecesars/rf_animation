03a48f27a8d24a0f4e4689ca9cd3ca154cf2b885
gecesars@gmail.com <gecesars@gmail.com>
2025-08-19 13:27:22 -0300

    novo

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: 'es2020',
+    cssCodeSplit: false,
+    assetsInlineLimit: 100_000_000,
+    rollupOptions: { output: { manualChunks: undefined } },
+  },
 })
